519d88cd09947d90d30a4d3d246341f2bedc8b4f
NodirUmarov <mr.umarov.1995@gmail.com>
2022-07-23 01:34:16 +0500

    Repositories configured

--- domain/src/main/java/com/epam/domain/repository/GiftCertificateRepository.java
+++ domain/src/main/java/com/epam/domain/repository/GiftCertificateRepository.java
@@ -1,10 +1,14 @@
 package com.epam.domain.repository;
 
 import com.epam.domain.entity.certificate.GiftCertificate;
+import com.epam.domain.entity.certificate.Tag;
+import java.util.Optional;
+import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GiftCertificateRepository extends JpaRepository<GiftCertificate, Long> {
-
+    Optional<GiftCertificate> findByCertificateName(String name);
+    Set<GiftCertificate> findByTags_TagName(String tagName);
 }
