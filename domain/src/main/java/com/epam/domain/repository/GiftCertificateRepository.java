91c75cffbb598f23577747ac2fc09c3dc147273f
NodirUmarov <mr.umarov.1995@gmail.com>
2022-07-23 02:10:30 +0500

    Pagination added to repository

--- domain/src/main/java/com/epam/domain/repository/GiftCertificateRepository.java
+++ domain/src/main/java/com/epam/domain/repository/GiftCertificateRepository.java
@@ -4,11 +4,13 @@ import com.epam.domain.entity.certificate.GiftCertificate;
 import com.epam.domain.entity.certificate.Tag;
 import java.util.Optional;
 import java.util.Set;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GiftCertificateRepository extends JpaRepository<GiftCertificate, Long> {
     Optional<GiftCertificate> findByCertificateName(String name);
-    Set<GiftCertificate> findByTags_TagName(String tagName);
+    Page<GiftCertificate> findByTags_TagName(Pageable pageable);
 }
